e1dc374f37578e99ce2998a439cf3121488c62aa
WangLei <15112125687@163.com>
2019-04-21 14:49:35 +0800

    提交测试6

--- e3_manager/e3_manager_web/src/main/java/com/wl/e3/test/Demo1.java
+++ e3_manager/e3_manager_web/src/main/java/com/wl/e3/test/Demo1.java
@@ -3,6 +3,6 @@ package com.wl.e3.test;
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("test4");
+		System.out.println("test6");
 	}
 }
