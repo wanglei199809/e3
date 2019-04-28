4bf3855d9e917b4dd0be3f1199e8ed5f041e022b
WangLei <15112125687@163.com>
2019-04-28 20:24:42 +0800

    添加注解

--- e3_parent/e3_portal_web/src/main/java/com/e3mall/portal/controller/IndexController.java
+++ e3_parent/e3_portal_web/src/main/java/com/e3mall/portal/controller/IndexController.java
@@ -3,9 +3,21 @@ package com.e3mall.portal.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 首页展示控制器
+ * <p>Title: IndexController</p>
+ * <p>@author WangLei </p>
+ * <p>@date 2019年4月28日 下午8:23:48</p>
+ * @version 1.0
+ */
 @Controller
 public class IndexController {
 
+	/**
+	 * 跳转到首页
+	 * <p>Title: showIndex</p>
+	 * <p>@date 2019年4月28日 下午8:23:58</p>
+	 */
 	@RequestMapping("/index")
 	public String showIndex() {
 		return "index";
